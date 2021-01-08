2da1f0ef8e10ff46e65cb92aee4653440c443d82
Max Roche <maxproche@gmail.com>
2021-01-08 14:41:09 -0600

    Basic site to track our current gr winnings

---
+++ index.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", fetchBitcoinPrice)
+
+let initialDate = "2021-01-08"
+let initialPurchasePrice = 40759.48
+let bitcoinOwned = 0.029441
+
+let url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+
+function fetchBitcoinPrice() {
+  fetch(url)
+    .then( (response) => response.json() )
+    .then( (jsonData) => jsonData["bpi"] )
+    .then( (bpiData) =>  bpiData["USD"] )
+    .then( usdData => usdData.rate )
+    .then( rateString => {
+      let rateWithoutCommas = rateString.replace(",", "")
+      let rateValue = parseFloat(rateWithoutCommas)
+      let currentPrice = bitcoinOwned * rateValue
+
+      let isGain = currentPrice - initialPurchasePrice > 0
+      let styleColor = isGain ? "green" : "red"
+
+      let element = document.getElementById("gr-pot-price")
+      element.innerHTML = "$" + currentPrice
+      element.style.color = styleColor
+    })
+}
